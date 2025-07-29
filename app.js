5e1bdafee590b4dc4abf5f9f10c66e2254f0ed18
donosty <mzac0077@gmail.com>
2025-07-29 12:05:16 -0600

    Primer avance

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+const botonAnadir = document.querySelector('#add-amigo')
+const alerta = document.querySelector('#alerta')
+const seccionAmigos = document.querySelector('#listaAmigos')
+
+cargarEventListeners();
+function cargarEventListeners(){
+
+    botonAnadir.addEventListener('click', anadirAmigo)
+}
+
+function anadirAmigo(e){
+    
+    const contenedor = e.target.parentElement
+    
+    let nombre = contenedor.querySelector('#amigo')
+    
+    if(nombre.value.trim() == ''){
+
+        crearAlerta()
+        return
+    }
+
+    amigos.push(nombre.value)
+    nombre.value = ''
+    mostrarAmigos()
+
+}
+
+function crearAlerta(){
+
+    alerta.classList.remove('oculto')
+
+    setTimeout(() =>{
+
+        alerta.classList.add('oculto')
+    },2000)
+}
+
+function mostrarAmigos(){
+
+    const li = document.createElement('li')
+
+    for(const amigo of amigos){
+
+        li.textContent = amigo
+        seccionAmigos.appendChild(li)
+    }
+
+}
+
+function sortearAmigo(){
+    
+}
